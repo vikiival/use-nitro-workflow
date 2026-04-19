33bf6b6751dcac03cf37a16bc1fcca6c3a3606e4
Viki Val <viktorko99@gmail.com>
2026-04-19 21:21:52 +0200

    :tada: real nitro v3

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -1,11 +1,18 @@
-import { eventHandler } from "h3"
+import { defineHandler } from "nitro"
 
-// Learn more: https://nitro.build/guide/routing
-export default eventHandler((event) => {
-  return `
-      <meta charset="utf-8">
-      <h1>This is your brand new Nitro project 🚀 </h1>
-      <p>Get started by editing the <code>server/routes/index.ts</code> file.</p>
-      <p>Learn more from 📖 <a href="https://nitro.build/guide" target="_blank">Nitro Documentation</a></p>
-    `;
+export default defineHandler((event) => {
+  event.res.headers.set("Content-Type", "text/html; charset=utf-8");
+  return `<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Nitro</title>
+</head>
+<body>
+  <h1>This is your brand new Nitro project 🚀</h1>
+  <p>Get started by editing the <code>server/routes/index.ts</code> file.</p>
+  <p>Learn more from 📖 <a href="https://nitro.build/guide" target="_blank">Nitro Documentation</a></p>
+</body>
+</html>`;
 });
